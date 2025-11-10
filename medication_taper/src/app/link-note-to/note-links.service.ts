ab9dd3c757062a1dfc8a769951b959ec1a9d8fe2
BrianKLeman <brian.leman@hotmail.co.uk>
2025-11-10 16:20:17 +0000

    Improving coding standards

--- medication_taper/src/app/link-note-to/note-links.service.ts
+++ medication_taper/src/app/link-note-to/note-links.service.ts
@@ -1,35 +1,37 @@
 import { Injectable } from '@angular/core';
-import { ProjectsService } from '../projects-table/projects.service';
-import { LearningAimsService } from '../learning-aims-table/learning-aims.service';
-import { SleepsService } from '../sleeps-table/sleeps.service';
-import { PhenomenaService } from '../phenomena-table/phenomena.service';
 import { HttpClient } from '@angular/common/http';
 import { UrlsService } from 'src/urls.service';
-import { AppointmentsService } from '../appointments-table/appointments.service';
 import { LinksService } from '../link-task-to/links.service';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteLinksService {
 
-  constructor(
-               private httpClient : HttpClient,
-              private urlsService : UrlsService,
-              private linksService : LinksService  ) { }
+  constructor(private readonly httpClient : HttpClient,
+              private readonly urlsService : UrlsService,
+              private readonly linksService : LinksService  ) { }
 
   public async getRecords(recordType : string){
     return await this.linksService.getRecords(recordType);   
   }
 
-  public AddLink(noteIDs : number[], tableName : string, entityID : number){
-    this.httpClient.post<number>( 
-      this.urlsService.GetApiURL()+"Api/NoteLinks",
-      { 
-        NoteIDs : noteIDs,
-        Table : tableName,
-        EntityID : entityID
-      }
-    ).toPromise().then( (n) => { console.log(`written note links with id ${n}`); });
+  public async AddLink(noteIDs : number[], tableName : string, entityID : number){
+    try
+    {      
+      const requestBody =  { 
+          NoteIDs : noteIDs,
+          Table : tableName,
+          EntityID : entityID
+        }
+      const result = await firstValueFrom(this.httpClient.post<number>( `${this.urlsService.GetApiURL()}Api/NoteLinks`, requestBody ));
+      console.log(`written note links with id ${result}`);
+    } 
+    catch ( error){
+      console.error("Error adding note link ", error);
+      throw error;
+    }
+
   }
 }
