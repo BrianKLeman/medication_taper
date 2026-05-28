c542b3040690842f85d6aefc8beb9aeb478d2dae
BrianKLeman <brian.leman@hotmail.co.uk>
2026-05-28 16:47:23 +0100

    urls service

--- medication_taper/src/urls.service.ts
+++ medication_taper/src/urls.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from './environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,8 @@ export class UrlsService {
 
   // Get api url with the / appended.
   public GetApiURL(){
-    //return "https://api.codefusionstudios.co.uk/";
-    //return "http://localhost:56265/";
-    return "https://localhost:7170/";
-    //return "https://46.252.195.64/";
+    
+    return environment.apiUrl;
 
   }
 }
