1363366c76b173414d381a38a23dca87835ee695
BrianKLeman <brian.leman@hotmail.co.uk>
2026-06-07 21:06:39 +0100

    change prescription type in add-medication

--- medication_taper/src/app/add-medication/add-medication.component.ts
+++ medication_taper/src/app/add-medication/add-medication.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { IMedication, MedicationDosesService } from '../medication-doses-table/medication-doses.service';
 import { TimezonesService } from '../timezones.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { IPrescription, PrescriptionsService } from '../prescriptions-table/prescriptions.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-add-medication',
@@ -9,21 +11,23 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
     styleUrls: ['./add-medication.component.css'],
     standalone: false
 })
-export class AddMedicationComponent {
+export class AddMedicationComponent implements OnInit {
 
 public constructor(@Inject(MAT_DIALOG_DATA) 
-        public data: 
-        {
-          prescriptionId: number,
-          dose : IMedication | null
-        },
-    private service : MedicationDosesService,
-    private timeService : TimezonesService   
+                public data: 
+                {
+                  prescriptionId: number,
+                  dose : IMedication | null
+                },
+                private service : MedicationDosesService,
+                private timeService : TimezonesService,
+                private medicationService : PrescriptionsService 
                ){
 
       if(!data.dose){
         let now = new Date(Date.now());
         this.currentDose = this.CreateNewDose(now, data.prescriptionId);
+        
       } else {
         this.currentDose = data.dose;
         
@@ -32,6 +36,16 @@ public constructor(@Inject(MAT_DIALOG_DATA)
       }
       
   }
+  
+  prescriptionFormControl = new FormControl();
+  async ngOnInit() {
+    this.prescriptions = await this.medicationService.getAllPrescriptionsForPerson();
+    this.selectedPrescriptionId = this.currentDose.PrescriptionId;
+    this.setSelectedPrescription(this.selectedPrescriptionId);
+  }
+
+
+  prescriptions : IPrescription[] = [];
 
   private CreateNewDose(now : Date, prescriptionId : number) : IMedication {    
     this.setDateAndTime(now);
@@ -61,11 +75,26 @@ public constructor(@Inject(MAT_DIALOG_DATA)
         this.date = d.toISOString();
   }
 
+  selectedPrescription : IPrescription | null = null;
+  selectedPrescriptionId = 0;
   private updateDose(n : IMedication){    
     n.DateTimeConsumed = this.date;
-    n.DoseTakenMG = (this.doseWeight_mg / this.tabletWeight_mg)*7.5;
+    let selectedMg = 0;
+    let prescriptionId = 0;
+    this.setSelectedPrescription(this.selectedPrescriptionId);
+    if( this.selectedPrescription ){
+      selectedMg = this.selectedPrescription.DoseMG;
+      prescriptionId = this.selectedPrescription.Id;
+    }
+    n.DoseTakenMG = (this.doseWeight_mg / this.tabletWeight_mg)*selectedMg;
+    n.PrescriptionId = this.selectedPrescriptionId;
   }
 
   tabletWeight_mg : number = 0;
   doseWeight_mg : number = 0;
+
+  setSelectedPrescription(prescriptionId : number){
+    
+    this.selectedPrescription = this.prescriptions.find( x => x.Id == prescriptionId) ?? null;
+  }
 }
